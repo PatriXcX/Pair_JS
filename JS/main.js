75854991bab0d57b2930127b590a7792f194a3f5
PatriXcX <patriciapicogarcia@gmail.com>
2024-05-25 15:37:13 +0200

    end of exercise 2

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,12 @@
 const text= document.querySelector('.js__hola');
 text.innerHTML= '<p>Hola Mundo</p> ';
 console.log('js__hola');
+
+//-------------------------------------
+
+const firstName= document.querySelector('.js__firstname');
+const Tittle= document.querySelector('.js__tittle');
+
+Tittle.innerHTML='La Adalaber seleccionada es: ' + firstName.innerHTML;
+//firstName.innerHTML= '<h2>La Adalaber seleccionada es:</h2>  ';
+console.log('js__firstname');
